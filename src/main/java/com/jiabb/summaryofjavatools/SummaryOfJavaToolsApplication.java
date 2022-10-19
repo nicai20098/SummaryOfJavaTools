2c96157c487364fca13d166a88f8e1c5d10d2399
jiabb-b <jiabb-b@glodon.com>
2022-10-19 15:49:42 +0800

    project init

---
+++ src/main/java/com/jiabb/summaryofjavatools/SummaryOfJavaToolsApplication.java
@@ -0,0 +1,13 @@
+package com.jiabb.summaryofjavatools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SummaryOfJavaToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SummaryOfJavaToolsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jiabb/summaryofjavatools/SummaryOfJavaTools1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jiabb.summaryofjavatools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummaryOfJavaTools1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
